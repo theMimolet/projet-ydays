441b4c017646ab79be8e3f955a92fcc768c2e8d5
Jeremy <deveneyjeremy0@gmail.com>
2025-11-19 16:03:57 +0100

    Mise en place dialogic + interraction

--- Scripts/character_body_2d.gd
+++ Scripts/character_body_2d.gd
@@ -8,6 +8,10 @@ const JUMP_VELOCITY = -400.0
 func _ready() -> void:
 	$AnimatedSprite2D.play("new_animation")
 
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("Interract"):
+		InteractionManager.handle_interaction(global_position)
+
 func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
