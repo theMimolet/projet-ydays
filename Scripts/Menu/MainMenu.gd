d4f9187035c1b3975ead805b98093d4f7c16730f
Mimolet <themimolet@proton.me>
2026-01-15 16:21:33 +0100

    feat: ajout de musique et menu options

--- Scripts/Menu/MainMenu.gd
+++ Scripts/Menu/MainMenu.gd
@@ -1,19 +1,34 @@
 extends MenuSwitcher
 
 enum menuState {MAIN, OPTIONS}
-#@onready var join_values: NodePath = "MenuContainer/Join/Values"
 
 func _ready() -> void:
-	$VersionMargin/Version.text = ProjectSettings.get_setting("application/config/version")
-	pass
+	GameSettings.window_mode_changed.connect(OnFullscreenChanged)
+	OnFullscreenChanged(GameSettings.isFullscreen())
+	$VersionContainer/Version.text = ProjectSettings.get_setting("application/config/version")
 
 func setup_switcher() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 	default_index = menuState.MAIN
-	#$VersionContainer/Version.text = "v" + ProjectSettings.get_setting("application/config/version")
 
 func _on_jouer_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Overworld.tscn")
 
+func _on_paramètres_pressed() -> void:
+	switch_to(menuState.OPTIONS)
+	
 func _on_quitter_pressed() -> void:
 	get_tree().quit()
+
+func _on_fullscreen_pressed() -> void:
+	GameSettings.toggleFullscreen()
+	OnFullscreenChanged(GameSettings.isFullscreen())
+
+func OnFullscreenChanged(isFullscreen: bool) -> void:
+	if isFullscreen : 
+		$MenuContainer/Options/Fullscreen.text = "Fullscreen : On"
+	else : 
+		$MenuContainer/Options/Fullscreen.text = "Fullscreen : Off"
+
+func _on_return_pressed() -> void:
+	switch_to(menuState.MAIN)
