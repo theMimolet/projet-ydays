3017a0dc11f3163ad30306d8836367fae094cbcb
polmzenou <paul.mehr68@gmail.com>
2026-03-18 09:46:59 +0100

    sprite arme combat view

--- Scripts/UI/DevConsole.gd
+++ Scripts/UI/DevConsole.gd
@@ -234,5 +234,6 @@ func _register_commands() -> void:
 	
 	# Armes
 	registry.register_command("give_weapon", ConsoleCommands.cmd_give_weapon, "Donne une arme", 1, 1, "give_weapon <dague|epee1|epee2|epee3|bloodsword>")
+	registry.register_command("give_all_weapons", ConsoleCommands.cmd_give_all_weapons, "Donne toutes les armes", 0, 0, "give_all_weapons")
 	registry.register_command("equip_weapon", ConsoleCommands.cmd_equip_weapon, "Equipe une arme", 1, 1, "equip_weapon <dague|epee1|epee2|epee3|bloodsword>")
 	registry.register_command("list_weapons", ConsoleCommands.cmd_list_weapons, "Liste les armes", 0, 0, "list_weapons")
